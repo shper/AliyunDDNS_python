485b1fa94dc0fc74ab0dbefc08e0c704616cee0e
Shper <me@shper.cn>
2021-11-18 19:05:40 +0800

    MDF 使用 Aliyun sdk

--- aliyun_core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .aliyun_client import *
-from .aliyun_request import *
-
-__all__ = ('AliyunClient', 'AliyunRequest')
-
-__version__ = "1.0.0"
--- aliyun_ddns.py
+++ aliyun_ddns.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-from aliyun_core import *
+import json
+import sys
+import pip._internal
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.auth.credentials import AccessKeyCredential
+from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import UpdateDomainRecordRequest
 
 # ----- Config Setting -----
 ACCESS_KEY_ID = '<your-access-key-id>'
@@ -40,7 +46,8 @@ MY_IPV4_ECHO_URL = "https://ipv4.ipw.cn/api/ip/myip"
 
 class AliyunDDNS:
     def __init__(self):
-        self._client = AliyunClient(ACCESS_KEY_ID, ACCESS_KEY_SECRET)
+        _credential = AccessKeyCredential(ACCESS_KEY_ID, ACCESS_KEY_SECRET)
+        self._client = AcsClient(region_id='cn-hangzhou', credential=_credential)
 
     def update_record(self):
         for domian in DOMIANS:
@@ -59,34 +66,47 @@ class AliyunDDNS:
                 print("Message: Need not to update, current IP: " + current_ip)
     
     def describe_domain_records(self, domainName: str, rr: str, type: str):
-        _request = AliyunRequest("POST", "DescribeDomainRecords")
-        _request.put_params("DomainName", domainName)
-        _request.put_params("RRKeyWord", rr)
-        _request.put_params("Type", type)
+        _request = DescribeDomainRecordsRequest()
+        _request.set_accept_format('json')
+        _request.set_DomainName(domainName)
+        _request.set_RRKeyWord(rr)
+        _request.set_Type(type)
 
-        _response = self._client.execute(_request)
-        _result = _response["DomainRecords"]["Record"]
-        for record in _result:
+        _response = self._client.do_action_with_exception(_request)
+        _response = json.loads(str(_response, encoding='utf-8'))
+
+        _records = _response["DomainRecords"]["Record"]
+        for record in _records:
             if record["DomainName"] == domainName and record["RR"] == rr and record["Type"] == type:
                 return record
 
-    def update_domain_record(self, recordId: str, rr: str, type: str, value:str, line:str = "default"):
-        _request = AliyunRequest("POST", "UpdateDomainRecord")
-        _request.put_params("RecordId", recordId)
-        _request.put_params("RR", rr)
-        _request.put_params("Type", type)
-        _request.put_params("Value", value)
-        _request.put_params("Line", line)
+    def update_domain_record(self, recordId: str, rr: str, type: str, value: str, line: str = "default"):
+        _request = UpdateDomainRecordRequest()
+        _request.set_RecordId(recordId)
+        _request.set_RR(rr)
+        _request.set_Type(type)
+        _request.set_Value(value)
+        _request.set_Line(line)
         
-        _response = self._client.execute(_request)
-        print(_response)
+        _response = self._client.do_action_with_exception(_request)
+        print("Result message: " + str(_response, encoding='utf-8'))
 
     def get_ip_address(self, url: str) ->str:
-        _response = self._client.get(url, timeout=15)
+        _response = self._client.session.get(url, timeout=15)
         if _response.status_code == 200:
             return _response.text.strip()
         else:
             raise Exception("The current IP address cannot be obtained. exitting...")
 
+
+class Setup:
+    @staticmethod
+    def install(package: str):
+        pip._internal.main(["install", package])
+
+
 if __name__ == '__main__':
-    AliyunDDNS().update_record()
+    if sys.argv.__len__() > 1 and sys.argv[1] == 'install':
+        Setup.install("aliyun-python-sdk-alidns==2.6.32")
+    else:
+        AliyunDDNS().update_record()
